74720b213e13f139e4c99f66368c1181cb4bb800
Gabriel Lebec <glebec@gmail.com>
2019-06-27 20:32:56 -0400

    chore(gallery): presentational component

--- client/components/gallery/all-pins.jsx
+++ client/components/gallery/all-pins.jsx
@@ -1,17 +1,8 @@
 /* eslint-disable react/no-did-mount-set-state */
 import React from 'react'
 import axios from 'axios'
-import MasonryLayout from 'react-masonry-layout'
 
-import Pin from './pin'
-
-const Loader = () => (
-    <section className="section">
-        <progress className="progress is-small is-danger" max="100">
-            Loading
-        </progress>
-    </section>
-)
+import Gallery from './gallery'
 
 class AllPins extends React.Component {
     constructor(props) {
@@ -35,30 +26,7 @@ class AllPins extends React.Component {
     }
 
     render() {
-        return (
-            <section className="section">
-                {this.state.loading ? <Loader /> : null}
-                <div className="columns is-centered">
-                    <div className="column is-narrow has-text-centered">
-                        <MasonryLayout
-                            id="masonry-layout"
-                            position={false}
-                            sizes={[
-                                { columns: 2, gutter: 20 },
-                                { mq: '880px', columns: 3, gutter: 20 },
-                                { mq: '1070px', columns: 4, gutter: 20 },
-                                { mq: '1300px', columns: 5, gutter: 20 },
-                            ]}
-                            infiniteScrollDisabled
-                        >
-                            {this.state.pins.map(pin => (
-                                <Pin key={pin.id} pin={pin} />
-                            ))}
-                        </MasonryLayout>
-                    </div>
-                </div>
-            </section>
-        )
+        return <Gallery pins={this.state.pins} loading={this.state.loading} />
     }
 }
 
--- client/components/gallery/gallery.jsx
+++ client/components/gallery/gallery.jsx
@@ -3,12 +3,6 @@ import MasonryLayout from 'react-masonry-layout'
 
 import Pin from './pin'
 
-const randomItems = () =>
-    Array.from({ length: 25 }, () => ({
-        id: Math.floor(Math.random() * 100000),
-        height: 300 + (Math.floor(Math.random() * 200) - 100),
-    }))
-
 const Loader = () => (
     <section className="section">
         <progress className="progress is-small is-danger" max="100">
@@ -17,52 +11,29 @@ const Loader = () => (
     </section>
 )
 
-class Gallery extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            pins: randomItems(),
-            loading: false,
-        }
-    }
-
-    loadItems = () => {
-        this.setState({ loading: true })
-        setTimeout(() => {
-            this.setState(oldState => ({
-                pins: [...oldState.pins, ...randomItems()],
-                loading: false,
-            }))
-        }, 1000)
-    }
-
-    render() {
-        return (
-            <section className="section">
-                <div className="columns is-centered">
-                    <div className="column is-narrow has-text-centered">
-                        <MasonryLayout
-                            id="masonry-layout"
-                            position={false}
-                            sizes={[
-                                { columns: 2, gutter: 20 },
-                                { mq: '880px', columns: 3, gutter: 20 },
-                                { mq: '1070px', columns: 4, gutter: 20 },
-                                { mq: '1300px', columns: 5, gutter: 20 },
-                            ]}
-                            infiniteScroll={this.loadItems}
-                            infiniteScrollLoading={this.state.loading}
-                            infiniteScrollSpinner={<Loader />}
-                        >
-                            {this.state.pins.map(pin => (
-                                <Pin key={pin.id} pin={pin} />
-                            ))}
-                        </MasonryLayout>
-                    </div>
-                </div>
-            </section>
-        )
-    }
-}
+const Gallery = ({ pins, loading }) => (
+    <section className="section">
+        <div className="columns is-centered">
+            <div className="column is-narrow has-text-centered">
+                <MasonryLayout
+                    id="masonry-layout"
+                    position={false}
+                    sizes={[
+                        { columns: 2, gutter: 20 },
+                        { mq: '880px', columns: 3, gutter: 20 },
+                        { mq: '1070px', columns: 4, gutter: 20 },
+                        { mq: '1300px', columns: 5, gutter: 20 },
+                    ]}
+                    infiniteScrollLoading={loading}
+                    infiniteScrollSpinner={<Loader />}
+                >
+                    {pins.map(pin => (
+                        <Pin key={pin.id} pin={pin} />
+                    ))}
+                </MasonryLayout>
+            </div>
+        </div>
+    </section>
+)
 
 export default Gallery
